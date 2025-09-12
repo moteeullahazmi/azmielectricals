f8e690e6e7eddb2a7844d3afb714b4a8530d9170
moteeullahazmi <moteeullahazmi@gmail.com>
2025-09-13 04:34:30 +0530

    adding button tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
+"use client"
 
 export default function Home() {
+  
+   const Homehandle =  () => {
+      console.log("Home Page Working")
+    }
+
   return (
-    <>
-    <div>Azmi</div>
-    <div>MOteeullah Azmi</div>
-    
-    </>
+    <div>
+      <button onClick={Homehandle}>Home</button>
+      <button>Products</button>
+      <button>About Us</button>
+      <button>Contact</button>
+    </div>
   );
 }
