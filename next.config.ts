66b6c0db95b4e872acf12ebcd2cb532e3eed33e3
moteeullahazmi <moteeullahazmi@gmail.com>
2025-09-13 12:16:03 +0530

    next-config code

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-   reactStrictMode: true,
-  images: {
-    domains: ["your-image-host.com"], // agar external images use ho rahi ho
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
-
-export default nextConfig;
+module.exports = nextConfig;
