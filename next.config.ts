57423c84cac3cf9c67a76bd2cfa73614a21b9421
moteeullahazmi <moteeullahazmi@gmail.com>
2025-09-13 11:57:22 +0530

    final work completed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   reactStrictMode: true,
+  images: {
+    domains: ["your-image-host.com"], // agar external images use ho rahi ho
+  },
 };
 
 export default nextConfig;
